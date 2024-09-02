003f5847f67b09c856350eeb6537e2c127d1c78a
神战 <13399349884@163.com>
2024-09-02 15:20:41 +0800

    修改价格小数位数

--- src/elmclient/src/views/Selfpage.vue
+++ src/elmclient/src/views/Selfpage.vue
@@ -1,55 +1,9 @@
 <template>
-    <div class="wrapper">
-        <header>
-            <div class="user">
-                <img src="../assets/logo.png">
-                <p>用户名</p>
-            </div>
-        </header>
-        <ul class="useraction">
-            <li @click="userself_inf">
-                <p>用户基本信息</p>
-            </li>
-            <li>
-                <p>会员</p>
-            </li>
-        </ul>
-    </div>
+
 </template>
 
-<script scope>
-export default {
-    data() {
-        return {
-
-        }
-    },
-    methods: {
-        userself_inf() {
-            if (1 == 2) {
-
-            } else {
-                this.$router.push({ path: '/login' });
-            }
-        }
-    }
-}
+<script>
 </script>
 
-<style scoped>
-.wrapper {
-    width: 100%;
-    height: 100%;
-}
-
-
-.wrapper header {
-    width: 100%;
-    height: 15vw;
-    background-color: #0097FF;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-.wrapper header
+<style>
 </style>
