b0f0c83b79252fb3ba78ee07db26e4252f2c851e
神战 <13399349884@163.com>
2024-09-06 21:18:15 +0800

    回溯成功-目前是完善‘我的’页面和用户地址修改跳转完善状态

--- src/elmclient/src/components/backer.vue
+++ src/elmclient/src/components/backer.vue
@@ -1,36 +1,18 @@
 <template>
 
   <!-- 返回按钮 -->
-  <!-- <img @click="goBack" src="../assets/backer.png" alt="返回" class="back-button"> -->
-  <div @click="goBack" class="back-button">
-	  &lt;
-  </div>
+  <img @click="goBack" src="../assets/backer.png" alt="返回" class="back-button">
+  
 </template>
 
 <script>
 export default {
   name: 'Backer',
-  // data(){
-  //   return{
-  //     isselfpage: false
-  //   }
-  // },
   methods: {
     goBack() {
       this.$router.go(-1);  // 返回上一页
     }
   }
-  // , beforeRouteEnter(to, from, next) {
-  //   // from 是上一页面的路由对象
-  //   console.log('上一页面的路径:', from.fullPath); // 获取上一页面路径
-  //   if(from.fullPath == '/selfpage'){
-  //     this.isselfpage = true;
-  //   }
-  //   else {
-  //     this.isselfpage = false;
-  //   }
-  //   next();
-  // }
 }
 </script>
 
@@ -39,8 +21,9 @@ export default {
 .wrapper .back-button {
 	position: absolute;
 	left: 2vw;
+	top: 2vw;
+	width: 8vw;
+	height: 8vw;
 	cursor: pointer;
-	color: black;
-	font-size: 6vw;
 }
 </style>
