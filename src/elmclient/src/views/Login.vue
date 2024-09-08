8b352659471f8eb0059a1dc1a4f0d898974bc9b8
Balance4 <18722502597@163.com>
2024-09-08 10:02:46 +0800

    安装jsencrypt依赖

--- src/elmclient/src/views/Login.vue
+++ src/elmclient/src/views/Login.vue
@@ -39,43 +39,43 @@
 </template>
 
 <script>
-	import Footer from '../components/Footer.vue';
-	import JSEncrypt from 'jsencrypt';
+import Footer from '../components/Footer.vue';
+import JSEncrypt from 'jsencrypt';
 
-	export default {
-		name: 'Login',
-		data() {
-			return {
-				userId: '',
-				password: ''
+export default {
+	name: 'Login',
+	data() {
+		return {
+			userId: '',
+			password: ''
+		}
+	},
+	methods: {
+		login() {
+			if (this.userId === '') {
+				alert('手机号码不能为空！');
+				return;
 			}
-		},
-		methods: {
-			login() {
-				if (this.userId === '') {
-					alert('手机号码不能为空！');
+			if (this.password === '') {
+				alert('密码不能为空！');
+				return;
+			}
+
+			// 获取公钥并加密密码
+			this.$axios.get('/public-key').then(response => {
+				const publicKey = response.data;
+
+				// 使用公钥加密密码
+				const encryptor = new JSEncrypt();
+				encryptor.setPublicKey(publicKey);
+				const encryptedPassword = encryptor.encrypt(this.password);
+
+				if (!encryptedPassword) {
+					alert('密码加密失败，请稍后再试！');
 					return;
 				}
-				if (this.password === '') {
-					alert('密码不能为空！');
-					return;
-				}
-
-				// 获取公钥并加密密码
-				this.$axios.get('/public-key').then(response => {
-					const publicKey = response.data; 
-
-					// 使用公钥加密密码
-					const encryptor = new JSEncrypt();
-					encryptor.setPublicKey(publicKey);
-					const encryptedPassword = encryptor.encrypt(this.password);
-
-					if (!encryptedPassword) {
-						alert('密码加密失败，请稍后再试！');
-						return;
-					}
-					// alert(encryptedPassword);
-					// 发送加密后的密码进行登录
+				// alert(encryptedPassword);
+				// 发送加密后的密码进行登录
 				//登录请求
 				this.$axios.post('users/login', {
 					userId: this.userId,
@@ -96,150 +96,150 @@
 					console.error('获取公钥失败：', error);
 				});
 			}).catch(error => {
-					console.error('获取公钥失败：', error);
-				});
-			},
-			register() { // 修正了register函数位置，它应该在methods对象内
-				this.$router.push({
-					path: 'register'
-				});
-			}
+				console.error('获取公钥失败：', error);
+			});
 		},
-		components: {
-			Footer
+		register() { // 修正了register函数位置，它应该在methods对象内
+			this.$router.push({
+				path: 'register'
+			});
 		}
+	},
+	components: {
+		Footer
 	}
+}
 </script>
 
 <style scoped>
-	/****************** 总容器 ******************/
-	.wrapper {
-		width: 100%;
-		height: 100%;
-	}
+/****************** 总容器 ******************/
+.wrapper {
+	width: 100%;
+	height: 100%;
+}
 
-	/****************** header部分 ******************/
-	.wrapper header {
-		width: 100%;
-		height: 12vw;
-		background-color: #0097FF;
-		color: #fff;
-		font-size: 4.8vw;
+/****************** header部分 ******************/
+.wrapper header {
+	width: 100%;
+	height: 12vw;
+	background-color: #0097FF;
+	color: #fff;
+	font-size: 4.8vw;
 
-		position: fixed;
-		left: 0;
-		top: 0;
-		z-index: 1000;
+	position: fixed;
+	left: 0;
+	top: 0;
+	z-index: 1000;
 
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 
-	/****************** 表单部分 ******************/
-	.wrapper .form-box {
-		width: 100%;
-		margin-top: 12vw;
-	}
+/****************** 表单部分 ******************/
+.wrapper .form-box {
+	width: 100%;
+	margin-top: 12vw;
+}
 
-	.wrapper .form-box li {
-		box-sizing: border-box;
-		padding: 4vw 3vw 0 3vw;
-		display: flex;
-		align-items: center;
-	}
+.wrapper .form-box li {
+	box-sizing: border-box;
+	padding: 4vw 3vw 0 3vw;
+	display: flex;
+	align-items: center;
+}
 
-	.wrapper .form-box li .title {
-		flex: 0 0 18vw;
-		font-size: 3vw;
-		font-weight: 700;
-		color: #666;
-	}
+.wrapper .form-box li .title {
+	flex: 0 0 18vw;
+	font-size: 3vw;
+	font-weight: 700;
+	color: #666;
+}
 
-	.wrapper .form-box li .content {
-		flex: 1;
-	}
+.wrapper .form-box li .content {
+	flex: 1;
+}
 
-	.wrapper .form-box li .content input {
-		border: none;
-		outline: none;
-		width: 100%;
-		height: 4vw;
-		font-size: 3vw;
-	}
+.wrapper .form-box li .content input {
+	border: none;
+	outline: none;
+	width: 100%;
+	height: 4vw;
+	font-size: 3vw;
+}
 
-	.wrapper .button-login {
-		width: 100%;
-		box-sizing: border-box;
-		padding: 4vw 3vw 0 3vw;
-	}
+.wrapper .button-login {
+	width: 100%;
+	box-sizing: border-box;
+	padding: 4vw 3vw 0 3vw;
+}
 
-	.wrapper .button-login button {
-		width: 100%;
-		height: 10vw;
-		font-size: 3.8vw;
-		font-weight: 700;
-		color: #fff;
-		background-color: #38CA73;
-		border-radius: 4px;
+.wrapper .button-login button {
+	width: 100%;
+	height: 10vw;
+	font-size: 3.8vw;
+	font-weight: 700;
+	color: #fff;
+	background-color: #38CA73;
+	border-radius: 4px;
 
-		border: none;
-		outline: none;
-	}
+	border: none;
+	outline: none;
+}
 
-	.wrapper .button-register {
-		width: 100%;
-		box-sizing: border-box;
-		padding: 4vw 3vw 0 3vw;
-	}
+.wrapper .button-register {
+	width: 100%;
+	box-sizing: border-box;
+	padding: 4vw 3vw 0 3vw;
+}
 
-	.wrapper .button-register button {
-		width: 100%;
-		height: 10vw;
-		font-size: 3.8vw;
-		font-weight: 700;
-		/*与上面登陆按钮不同的只有颜色、背景色、边框不同*/
-		color: #666;
-		background-color: #EEE;
-		border: solid 1px #DDD;
-		border-radius: 4px;
+.wrapper .button-register button {
+	width: 100%;
+	height: 10vw;
+	font-size: 3.8vw;
+	font-weight: 700;
+	/*与上面登陆按钮不同的只有颜色、背景色、边框不同*/
+	color: #666;
+	background-color: #EEE;
+	border: solid 1px #DDD;
+	border-radius: 4px;
 
-		border: none;
-		outline: none;
-	}
+	border: none;
+	outline: none;
+}
 
-	/****************** 底部菜单部分 ******************/
-	.wrapper .footer {
-		width: 100%;
-		height: 14vw;
-		border-top: solid 1px #DDD;
-		background-color: #fff;
+/****************** 底部菜单部分 ******************/
+.wrapper .footer {
+	width: 100%;
+	height: 14vw;
+	border-top: solid 1px #DDD;
+	background-color: #fff;
 
-		position: fixed;
-		left: 0;
-		bottom: 0;
+	position: fixed;
+	left: 0;
+	bottom: 0;
 
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
-	}
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+}
 
-	.wrapper .footer li {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+.wrapper .footer li {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 
-		color: #999;
-		user-select: none;
-		cursor: pointer;
-	}
+	color: #999;
+	user-select: none;
+	cursor: pointer;
+}
 
-	.wrapper .footer li p {
-		font-size: 2.8vw;
-	}
+.wrapper .footer li p {
+	font-size: 2.8vw;
+}
 
-	.wrapper .footer li i {
-		font-size: 5vw;
-	}
+.wrapper .footer li i {
+	font-size: 5vw;
+}
 </style>
