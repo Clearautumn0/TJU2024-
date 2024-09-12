c003f9a360062995d57599b8f46a95e0b1b31f3c
Balance4 <18722502597@163.com>
2024-09-12 11:18:00 +0800

    删除log

--- src/elmclient/src/views/Selfpage.vue
+++ src/elmclient/src/views/Selfpage.vue
@@ -168,7 +168,7 @@ const refreshAuthorization = () => {
 
 onMounted(() => {
 	user.value = getSessionStorage('user') || { userName: '未登录', userId: '', userImg: '' };
-	console.log(user.value);
+	// console.log(user.value);
 	getBusinessId();
 	
 	if(user.value.userId!=''){
